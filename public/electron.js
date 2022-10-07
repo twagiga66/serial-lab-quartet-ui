c23ff4d92946704efa68cd8dd11f9296ab39622b
mmicherda <mmicherda@andea.com>
2022-10-07 15:53:21 +0200

    Upgrade electron.js

--- public/electron.js
+++ public/electron.js
@@ -117,8 +117,9 @@ if (process.env.REACT_DEV === "dev") {
 }
 
 async function createWindow() {
+  let splash;
   if(process.platform !== "darwin") {
-    let splash = require("./main-process/splash.js").renderSplashScreen();
+    splash = require("./main-process/splash.js").renderSplashScreen();
   }
   electron.session.defaultSession.webRequest.onHeadersReceived(
     (details, callback) => {
